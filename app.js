15dfbb99efbdbdde5abfd8d94c2c836f4ec13003
Nuno Neto <nuno.agn@gmail.com>
2015-09-21 22:15:27 +0100

    init repo

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+app.set('views','./public/views');
+app.set('view engine','ejs');
+
+app.use('/resources/libs',express.static('bower_components'));
+app.use('/resources',express.static('public/resources'));
+
+
+app.get('/',function(request, response){
+  response.render('index');
+});
+
+var server = app.listen(80,function(){
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("installIT started, listening on port "+port);
+});
